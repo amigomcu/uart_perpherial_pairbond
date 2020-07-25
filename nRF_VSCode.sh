d7fd02dc51f84309cfd026a018dffa45dfc7dd35
林小平 <linxiaoping@linxiaopingdeMacBook-Pro.local>
2020-07-25 23:00:43 +0800

    init

---
+++ nRF_VSCode.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+export nRF_SDK="/Users/linxiaoping/nrf5x/nRF5_SDK_15.3.0_59ac345"
+export GNU_GCC="/Users/linxiaoping/gun/gcc-arm-none-eabi-7-2018-q2-update"
+open "/Applications/Visual Studio Code.app" ble_uart.code-workspace
